561627c94ec86206ae2b5874a361d7bbc84590a7
AIDEN Cloud & Security Consulting <devsecops@aiden-it-consulting.com>
2026-09-24 15:13:11 +0200

    twn-devops-bootcamp - cleanup Jenkinsfile2 and bind with script

--- script.groovy
+++ script.groovy
@@ -1,6 +1,23 @@
-def buildApp() {
+def buildJar() {
     echo 'Building the Application ..........'
-    echo "Building ${params.APP_NAME} version ${params.APP_VERSION} .........."
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo 'Building the docker image...........'
+    withCredentials(
+            [
+                    usernamePassword(
+                            credentialsId: 'docker-hub-repo-asambataiden',
+                            passwordVariable: 'DOCKERHUB_PASSWORD',
+                            usernameVariable: 'DOCKERHUB_USERNAME')
+            ]
+    )
+            {
+                sh 'docker build -t asambataiden/demo-app:2.0 .'
+                sh 'echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin'
+                sh 'docker push asambataiden/demo-app:2.0'
+            }
 }
 
 def testApp() {
