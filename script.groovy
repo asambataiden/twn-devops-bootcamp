58105ab675bd3fc1335afa47f440630ab707e851
AIDEN Cloud & Security Consulting <devsecops@aiden-it-consulting.com>
2026-09-24 12:58:28 +0200

    twn-devops-bootcamp - cleaning and optimising Jenkinsfile1 with external groovy script

---
+++ script.groovy
@@ -0,0 +1,22 @@
+def buildApp() {
+
+    echo 'Building the Application ..........'
+    echo "Building ${params.APP_NAME} version ${params.APP_VERSION} .........."
+}
+
+
+def testApp() {
+
+    echo 'Testing the Application ..........'
+    echo "Testing ${params.APP_NAME} version ${params.APP_VERSION} .........."
+}
+
+
+
+def deployApp() {
+
+    echo 'Deploying the Application ..........'
+    echo "Deploying ${params.APP_NAME} version ${params.APP_VERSION} .........."
+}
+
+return this
